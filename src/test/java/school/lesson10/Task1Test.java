6a7ea7bb2efd03bbbfb1fc2593b1dcec51bf7c7a
pechenka9000 <pechenka9000@gmail.com>
2021-08-29 22:25:31 +0300

    Create test-case for lesson10 and create logs Author: Vladislav Gukov <pechenka9000@gmail.com> Date: Sun Aug 29 22:25:17 2021
    Create log4j function and creating logfile in package "log"
    
    Signed-off-by: Vladislav Gukov <pechenka9000@gmail.com>

--- src/test/java/school/lesson10/Task1Test.java
+++ src/test/java/school/lesson10/Task1Test.java
@@ -3,15 +3,19 @@ package school.lesson10;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.*;
 import static org.junit.Assert.*;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Task1Test {
     private WebDriver driver;
     private boolean acceptNextAlert = true;
     private final StringBuffer verificationErrors = new StringBuffer();
+    private static final Logger LOGGER = LoggerFactory.getLogger(Task1Test.class.getName());
 
     @BeforeClass
     public static void setupClass() {
@@ -25,45 +29,46 @@ public class Task1Test {
 
     @Test
     public void testTask1() {
-        WebDriverWait wait = new WebDriverWait(driver, 300);
+        WebDriverWait wait = new WebDriverWait(driver, 30);
         driver.get("https://habr.com/ru/all/");
         driver.manage().window().maximize();
-        System.out.println("Осуществлена загрузка сайта");
+        LOGGER.info("Осуществлена загрузка сайта");
         driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div[2]/div/div")).click();
-        System.out.println("Произведён клик на иконку профиля");
+        LOGGER.info("Произведён клик на иконку профиля");
         driver.findElement(By.linkText("Войти")).click();
-        System.out.println("Произведён клик по кнопке 'Войти'");
+        LOGGER.info("Произведён клик по кнопке 'Войти'");
         try {
             assertEquals("Вход", driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='E-mail'])[1]/preceding::div[1]")).getText());
         } catch (Error e) {
+            LOGGER.error("Такого элемента нет на данной странице ", e);
             verificationErrors.append(e);
         }
-        System.out.println("Верификация текста в заголовке");
+        LOGGER.info("Верификация текста в заголовке");
         driver.findElement(By.id("email_field")).click();
         driver.findElement(By.id("email_field")).clear();
         driver.findElement(By.id("email_field")).sendKeys("pechenka9000@gmail.com");
-        System.out.println("Введён @email");
+        LOGGER.info("Введён @email");
         driver.findElement(By.id("password_field")).click();
         driver.findElement(By.id("password_field")).clear();
         driver.findElement(By.id("password_field")).sendKeys("Zetju2mkyv");
-        System.out.println("Введён пароль");
+        LOGGER.info("Введён пароль");
         driver.findElement(By.xpath("//*[@id=\"login_form\"]/fieldset/div[4]/button")).click();
-        System.out.println("Нажата кнопка 'Вход'");
+        LOGGER.info("Нажата кнопка 'Вход'");
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='app']/div[1]/div[2]/div[2]/div/div/div[1]/div/nav/a[1]")));
         driver.findElement(By.xpath("//*[@id='app']/div[1]/div[2]/div[2]/div/div/div[1]/div/nav/a[1]")).click();
-        System.out.println("Выбрана вкладка 'Моя лента'");
+        LOGGER.info("Выбрана вкладка 'Моя лента'");
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"574528\"]/div[1]/h2/a")));
         driver.findElement(By.xpath("//*[@id=\"574528\"]/div[1]/h2/a")).click();
-        System.out.println("Открыт пост из ленты");
+        LOGGER.info("Открыт пост из ленты");
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/main/div/div/div[2]/div[1]/div/div[3]/div/div/button/span[1]")));
         driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/main/div/div/div[2]/div[1]/div/div[3]/div/div/button/span[1]")).click();
-        System.out.println("Пост добавлен в закладки");
+        LOGGER.info("Пост добавлен в закладки");
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div")));
         driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div")).click();
-        System.out.println("Осуществлён клик по кнопке профиля");
+        LOGGER.info("Осуществлён клик по кнопке профиля");
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[5]/a[2]")));
         driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[5]/a[2]")).click();
-        System.out.println("Осуществлён выход из профиля");
+        LOGGER.info("Осуществлён выход из профиля");
     }
 
     @After
